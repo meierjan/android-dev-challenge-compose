2633bb85d7b0203ae01caf242cb5ffe61236a54d
Jan Meier <jan@meier.wtf>
2021-02-28 12:39:08 +0100

    Very basic solution for the challenge

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,19 +18,63 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.*
 import com.example.androiddevchallenge.ui.theme.MyTheme
+
+data class Pet(
+    val id: Long,
+    val name: String
+)
+
+class PetsViewModel : ViewModel() {
+
+    private val petsDataStore: Map<Long, Pet> = hashMapOf(
+        0L to Pet(0, "Aaliyah"),
+        1L to Pet(1, "Asterix"),
+        2L to Pet(2, "Rocky"),
+        3L to Pet(3, "Mara")
+    )
+
+    val pets: List<Pet> = petsDataStore.map { it.value }
+
+    val pet: Map<Long, Pet> = petsDataStore
+
+
+}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyTheme {
-                MyApp()
+                val navController = rememberNavController()
+
+                NavHost(navController, startDestination = "/") {
+                    composable("/") { PetsOverviewPage(navController) }
+                    composable(
+                        "pet/{petId}",
+                        arguments = listOf(navArgument("petId") {
+                            type = NavType.LongType
+                        })
+                    ) { backStackEntry ->
+                        PetsDetailView(
+                            navController,
+                            backStackEntry.arguments?.getLong("petId")
+                        )
+                    }
+                }
             }
         }
     }
@@ -38,24 +82,31 @@ class MainActivity : AppCompatActivity() {
 
 // Start building your app here!
 @Composable
-fun MyApp() {
+fun PetsOverviewPage(navController: NavHostController) {
+
+    val viewModel: PetsViewModel = viewModel()
+
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+        LazyColumn {
+            itemsIndexed(viewModel.pets) { _, item ->
+                PetListView(item) { navController.navigate("pet/${item.id}") }
+            }
+        }
     }
 }
 
-@Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
-fun LightPreview() {
-    MyTheme {
-        MyApp()
-    }
+fun PetListView(pet: Pet, onPetClicked: () -> Unit) {
+    Text(text = pet.name, Modifier.clickable { onPetClicked() })
 }
 
-@Preview("Dark Theme", widthDp = 360, heightDp = 640)
 @Composable
-fun DarkPreview() {
-    MyTheme(darkTheme = true) {
-        MyApp()
+fun PetsDetailView(navController: NavHostController, petId: Long?) {
+
+    val viewModel: PetsViewModel = viewModel()
+    val pet = viewModel.pet[petId]!!
+    Surface(color = MaterialTheme.colors.background) {
+        Text(text = pet.name)
     }
+
 }
